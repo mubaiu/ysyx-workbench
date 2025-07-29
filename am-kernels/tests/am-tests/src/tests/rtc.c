75d4bac0bea0d374e64e38dbd604dc0ac22c21fd
tracer-ysyx <tracer@ysyx.org>
2025-07-29 20:24:00 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:24:00 up 11:17,  1 user,  load average: 1.13, 1.41, 1.13

--- am-kernels/tests/am-tests/src/tests/rtc.c
+++ am-kernels/tests/am-tests/src/tests/rtc.c
@@ -7,11 +7,11 @@ void rtc_test() {
     while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec) ;
     rtc = io_read(AM_TIMER_RTC);
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
-    if (sec == 1) {
-      printf("%d second).\n", sec);
-    } else {
-      printf("%d seconds).\n", sec);
-    }
+    // if (sec == 1) {
+    //   printf("%d second).\n", sec);
+    // } else {
+    //   printf("%d seconds).\n", sec);
+    // }
     // sec ++;
   }
 }
