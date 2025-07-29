da583a5dc1660afb7d32cf81837b23f4d1640306
tracer-ysyx <tracer@ysyx.org>
2025-07-29 20:34:06 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:34:06 up 11:27,  1 user,  load average: 0.75, 0.84, 0.94

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,17 +5,16 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // // uint32_t lo, hi;
+  // uint32_t lo, hi;
   
-  // // 然后读取高32位
-  // uint32_t hi = inl(RTC_ADDR + 4);
-  // // 首先读取低32位
-  // uint32_t lo = inl(RTC_ADDR);
+  // 然后读取高32位
+  uint32_t hi = inl(RTC_ADDR + 4);
+  // 首先读取低32位
+  uint32_t lo = inl(RTC_ADDR);
   
   
-  // // 组合成一个64位的微秒计数值
-  // uptime->us = ((uint64_t)hi << 32) | lo;
-  uptime->us = 50000000;
+  // 组合成一个64位的微秒计数值
+  uptime->us = ((uint64_t)hi << 32) | lo;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
