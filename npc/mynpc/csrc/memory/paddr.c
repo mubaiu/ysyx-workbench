5b586c8b0d53fc246708210705bedf5f23ab6d4b
tracer-ysyx <tracer@ysyx.org>
2025-07-31 10:48:07 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:48:07 up  1:44,  1 user,  load average: 0.37, 0.58, 0.50

--- npc/mynpc/csrc/memory/paddr.c
+++ npc/mynpc/csrc/memory/paddr.c
@@ -106,8 +106,8 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 static void out_of_bound(paddr_t addr) {
   // panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
   //     addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
-  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
-      addr, PMEM_LEFT, PMEM_RIGHT, npc.pc);
+  // panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+  //     addr, PMEM_LEFT, PMEM_RIGHT, npc.pc);
 }
 
 void init_mem() {
@@ -140,7 +140,7 @@ word_t paddr_read(paddr_t addr, int len) {
     }
   }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  // out_of_bound(addr);
+  out_of_bound(addr);
   
   return 0;
 }
@@ -161,5 +161,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   }
   
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  // out_of_bound(addr);
+  out_of_bound(addr);
 }
--- npc/mynpc/wave.fst
+++ npc/mynpc/wave.fst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f860e65abb8cb65cb240446dfe6bdbdad71483d3ae2bd311b94bafd345463a3
+oid sha256:434cf1701987b1fb381f5f821530423f5855529a6b67f6999d0cce8ff797845f
 size 389
