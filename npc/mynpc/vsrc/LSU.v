c17411fcc42344aef9f286d1fc484e35fb8cc5ab
tracer-ysyx <tracer@ysyx.org>
2025-07-31 14:20:42 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:20:42 up  5:17,  1 user,  load average: 0.73, 0.64, 0.51

--- npc/mynpc/vsrc/LSU.v
+++ npc/mynpc/vsrc/LSU.v
@@ -17,8 +17,8 @@ module LSU(
     output wire [31:0] ram_wdata
 );
 
-    import "DPI-C" function void paddr_write(input int addr, input int len, input int data);
-    import "DPI-C" function int paddr_read(input int addr, input int len);
+    import "DPI-C" function void vaddr_write(input int addr, input int len, input int data);
+    import "DPI-C" function int vaddr_read(input int addr, input int len);
 
     // RAM控制信号
     assign ram_we = mem_write;
@@ -33,25 +33,25 @@ module LSU(
             temp_data = 32'h0; // 默认值
             case (funct3)
                 3'b100: begin
-                    load_data = paddr_read(addr, 1);   //lbu
+                    load_data = vaddr_read(addr, 1);   //lbu
                 end
                 3'b101: begin
-                    load_data = paddr_read(addr, 2);   //lhu
+                    load_data = vaddr_read(addr, 2);   //lhu
                 end
                 3'b001: begin
-                    temp_data = paddr_read(addr, 2);
+                    temp_data = vaddr_read(addr, 2);
                     load_data = {{16{temp_data[15]}}, temp_data[15:0]};   //lh
                 end
-                default: load_data = paddr_read(addr, 4); // 从计算出的地址读取数据
+                default: load_data = vaddr_read(addr, 4); // 从计算出的地址读取数据
 
             endcase
         end 
         else if (mem_write) begin
             load_data = 32'h0; // 如果不是读操作，返回0
             case (funct3) 
-                3'b000: paddr_write(addr, 1, store_data); // sb
-                3'b001: paddr_write(addr, 2, store_data); // sh
-                default: paddr_write(addr, 4, store_data); // sw
+                3'b000: vaddr_write(addr, 1, store_data); // sb
+                3'b001: vaddr_write(addr, 2, store_data); // sh
+                default: vaddr_write(addr, 4, store_data); // sw
             endcase
         end
         else begin
--- npc/mynpc/wave.fst
+++ npc/mynpc/wave.fst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b74b5b853f581a3af2d6c96e82b2ca0bb98ed16fd63117fccc9130f2d07b2ac
-size 6778
+oid sha256:6b965daca80dbf4a767d8024bd90d718ed343e85b3696bb24618c20a1db2438c
+size 7442
