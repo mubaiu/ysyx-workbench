6840a09e08d61af4af9f986314ddd9dd2e81fcb5
tracer-ysyx <tracer@ysyx.org>
2025-07-31 10:48:40 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:48:40 up  1:45,  1 user,  load average: 1.40, 0.79, 0.57

--- abstract-machine/am/build/riscv32e-npc/src/riscv/npc/timer.d
+++ abstract-machine/am/build/riscv32e-npc/src/riscv/npc/timer.d
@@ -2,5 +2,4 @@
  /home/mubai/ysyx-workbench/abstract-machine/am/src/riscv/npc/timer.c \
  /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
  /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib.h
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h
