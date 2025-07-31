8b401e34cf5f6469b3eb52a455acc06692e74724
tracer-ysyx <tracer@ysyx.org>
2025-07-31 20:20:46 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:20:46 up 11:17,  1 user,  load average: 0.61, 0.54, 0.65

---
+++ fceux-am/build/riscv32e-npc/src/cart.d
@@ -0,0 +1,20 @@
+/home/mubai/ysyx-workbench/fceux-am/build/riscv32e-npc/src/cart.o: \
+ /home/mubai/ysyx-workbench/fceux-am/src/cart.cpp \
+ /home/mubai/ysyx-workbench/fceux-am/src/types.h \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/fceu.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/git.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/utils/md5.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/utils/../types.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/utils/valuearray.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/ppu.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/driver.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/file.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/emufile.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/cart.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/x6502.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/x6502struct.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/utils/memory.h
--- npc/mynpc/wave.fst
+++ npc/mynpc/wave.fst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c336827afa0ddc882b8cb23056817f94a9910ead7430fbe582791d1606f0147c
-size 9759615
+oid sha256:761859b4d99aa4a37f98e92166e1c22acde4816a11aabfa0d6946fbdc99811d1
+size 7967
