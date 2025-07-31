6ad3e59cbcf66cb566c9630571ff74e2570c91ad
tracer-ysyx <tracer@ysyx.org>
2025-07-31 19:11:13 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:11:13 up 10:07,  1 user,  load average: 0.49, 0.52, 0.53

--- npc/mynpc/csrc/memory/paddr.c
+++ npc/mynpc/csrc/memory/paddr.c
@@ -17,21 +17,10 @@
 #include <memory/paddr.h>
 #include <device/mmio.h>
 #include <isa.h>
-#include <stdio.h>
 #include <time.h>
-#include <stdint.h>
-#include <sys/time.h>
+#include <stdio.h>
 
-// uint64_t get_time() {
-//   struct timeval tv;
-//   gettimeofday(&tv, NULL);
-  
-//   // 将秒和微秒转换为64位微秒时间戳
-//   uint64_t us = tv.tv_sec * 1000000ULL + tv.tv_usec;
-//   return us;
-// }
-
-#define DEVICE_BASE 0xa0000000
+# define DEVICE_BASE 0xa0000000
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
 
--- npc/mynpc/wave.fst
+++ npc/mynpc/wave.fst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2f42eaeacb8a553e9e3053690efcb0be461cc82c15e79116d9558e0e2cac7ad
-size 295142
+oid sha256:06f0d4c87b0a6e58a3d1a7a0cee6eb25e687d30ccda0c167759f57def0067799
+size 344294
