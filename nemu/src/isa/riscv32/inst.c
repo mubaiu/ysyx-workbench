be358d2eacae1b0b80005d4af6d65d74f9806751
tracer-ysyx <tracer@ysyx.org>
2025-08-01 16:05:43 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:05:43 up  5:20,  1 user,  load average: 1.28, 1.13, 0.81

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -149,7 +149,7 @@ static int decode_exec(Decode *s) {
                                                                 }
                                                             );
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, word_t ecall_code = 11;  // Machine环境调用异常码
-                                                                // nemu_state.state = NEMU_STOP;
+                                                                nemu_state.state = NEMU_STOP;
                                                                 printf("ecall to pc=0x%08x\n", isa_raise_intr(ecall_code, s->pc));
                                                                 s->dnpc = isa_raise_intr(ecall_code, s->pc);
                                                                 );
