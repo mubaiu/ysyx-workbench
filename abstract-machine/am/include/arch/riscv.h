91a8ea28a24c91ca9f0a83161c7ffe22775f36e6
tracer-ysyx <tracer@ysyx.org>
2025-08-01 16:34:49 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:48 up  5:49,  1 user,  load average: 0.29, 0.50, 0.55

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -8,7 +8,8 @@
 #endif
 
 struct Context {
-  uintptr_t gpr[NR_REGS], mcause, mstatus, mepc;
+  // TODO: fix the order of these members to match trap.S
+  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
   void *pdir;
 };
 
