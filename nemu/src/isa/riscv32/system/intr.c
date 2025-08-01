d88e6c66e84109eadf16df47f024c9c0f8b2aca6
tracer-ysyx <tracer@ysyx.org>
2025-08-01 16:56:01 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:56:01 up  6:11,  1 user,  load average: 0.59, 0.67, 0.75

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -24,21 +24,27 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   // 保存中断/异常原因到mcause CSR
   cpu.csr.mcause = NO;
   
-  // 设置中断状态标志 - 只修改MIE和MPIE位，保持其他位不变
+  // 设置中断状态标志 - 将MIE位保存到MPIE位，然后清除MIE位
   uint32_t mstatus_val = cpu.csr.mstatus;
+  printf("isa_raise_intr: mstatus before=0x%x\n", mstatus_val);
   
-  // 保存当前MIE位到MPIE位
-  if (mstatus_val & (1 << 3)) {  // 如果当前MIE位为1
-    mstatus_val |= (1 << 7);     // 设置MPIE位为1
-  } else {
-    mstatus_val &= ~(1 << 7);    // 设置MPIE位为0
+  // 保存当前MIE位的状态
+  bool current_mie = (mstatus_val & (1 << 3)) != 0;
+  printf("isa_raise_intr: current_mie=%d\n", current_mie);
+  
+  // 清除MPIE位，然后根据保存的MIE位状态设置MPIE位
+  mstatus_val &= ~(1 << 7);        // 清除MPIE位
+  if (current_mie) {               // 如果原来的MIE位为1
+    mstatus_val |= (1 << 7);       // 设置MPIE位为1
   }
+  // 如果原来的MIE位为0，MPIE位保持0
   
   // 清除MIE位
   mstatus_val &= ~(1 << 3);
   
   // 更新mstatus寄存器
   cpu.csr.mstatus = mstatus_val;
+  printf("isa_raise_intr: mstatus after=0x%x\n", mstatus_val);
   
   // 确定异常处理程序入口地址
   // 根据mtvec的模式位(最低两位)决定入口地址
