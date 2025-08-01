69422597bb4b588287e1f365a419b7ee4d93e358
tracer-ysyx <tracer@ysyx.org>
2025-08-01 09:44:07 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:44:07 up 33 min,  1 user,  load average: 0.97, 1.01, 0.72

---
+++ fceux-am/build/riscv32e-npc/nes/gen/100in1.d
@@ -0,0 +1,2 @@
+/home/mubai/ysyx-workbench/fceux-am/build/riscv32e-npc/./nes/gen/100in1.o: \
+ /home/mubai/ysyx-workbench/fceux-am/nes/gen/100in1.c
---
+++ fceux-am/build/riscv32e-npc/src/unused.d
@@ -0,0 +1,8 @@
+/home/mubai/ysyx-workbench/fceux-am/build/riscv32e-npc/src/unused.o: \
+ /home/mubai/ysyx-workbench/fceux-am/src/unused.cpp \
+ /home/mubai/ysyx-workbench/fceux-am/src/types.h \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/mubai/ysyx-workbench/fceux-am/src/sound.h
