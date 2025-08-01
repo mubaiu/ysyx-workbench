8506c768b77fa4831fe415f699741d4ae5f94e05
tracer-ysyx <tracer@ysyx.org>
2025-08-01 09:27:49 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:27:49 up 16 min,  1 user,  load average: 0.52, 0.46, 0.33

--- npc/mynpc/csrc/monitor.c
+++ npc/mynpc/csrc/monitor.c
@@ -20,7 +20,8 @@ static void welcome() {
   Log("Itrace: %s", MUXDEF(CONFIG_ITRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));\
   Log("Ftrace: %s", MUXDEF(CONFIG_FTRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));\
   Log("Mtrace: %s", MUXDEF(CONFIG_MTRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));\
-  
+  Log("Wtrace: %s", MUXDEF(ENABLE_WAVE_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));\
+
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
