c6b0614eab17b7a49c56928392fc8a96ca02bcc0
tracer-ysyx <tracer@ysyx.org>
2025-08-01 19:57:37 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:37 up  9:12,  1 user,  load average: 0.65, 0.82, 0.69

--- am-kernels/kernels/hello/build/riscv32-nemu/etrace_test.d
+++
@@ -1,6 +0,0 @@
-/home/mubai/ysyx-workbench/am-kernels/kernels/hello/build/riscv32-nemu/etrace_test.o: \
- /home/mubai/ysyx-workbench/am-kernels/kernels/hello/etrace_test.c \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
