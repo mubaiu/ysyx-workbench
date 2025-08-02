68b0c51918c6961f1b984e43d96cd3a9ce240877
tracer-ysyx <tracer@ysyx.org>
2025-08-02 10:36:18 +0800

    > run NEMU ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:36:18 up  1:29,  1 user,  load average: 1.12, 0.86, 0.74

---
+++ am-kernels/kernels/yield-os/build/riscv32-nemu/yield-os.d
@@ -0,0 +1,6 @@
+/home/mubai/ysyx-workbench/am-kernels/kernels/yield-os/build/riscv32-nemu/yield-os.o: \
+ /home/mubai/ysyx-workbench/am-kernels/kernels/yield-os/yield-os.c \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
