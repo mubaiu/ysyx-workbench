041e489df4c67d2d4d1fdfce4caed60278984c61
tracer-ysyx <tracer@ysyx.org>
2025-08-07 14:26:01 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:01 up  5:25,  1 user,  load average: 1.11, 0.80, 0.52

--- am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/dummy.d
+++
@@ -1,2 +0,0 @@
-/home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/dummy.o: \
- /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/tests/dummy.c
--- am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/quick-sort.d
+++
@@ -1,8 +0,0 @@
-/home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/quick-sort.o: \
- /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/tests/quick-sort.c \
- /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/include/trap.h \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib.h \
- /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
