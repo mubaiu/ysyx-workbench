376af3cf9da50e085f6fd69fa18c8a8b1c513642
ysyx_25010003-彭柏淳 <2948889749@qq.com>
2025-08-07 09:33:37 +0800

    rtt

---
+++ am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/hello-str.d
@@ -0,0 +1,8 @@
+/home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/hello-str.o: \
+ /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/tests/hello-str.c \
+ /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/include/trap.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib.h \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
