2b94cc299338420e4b480f8d5ccbeeca08c8093f
tracer-ysyx <tracer@ysyx.org>
2025-08-07 14:25:04 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:04 up  5:24,  1 user,  load average: 2.07, 0.87, 0.52

---
+++ am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/crc32.d
@@ -0,0 +1,8 @@
+/home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/crc32.o: \
+ /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/tests/crc32.c \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib-macros.h \
+ /home/mubai/ysyx-workbench/am-kernels/tests/cpu-tests/include/trap.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/mubai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/mubai/ysyx-workbench/abstract-machine/klib/include/klib.h
--- npc/mynpc/wave.fst
+++ npc/mynpc/wave.fst
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf624de245a7a2e5b45a8096e68dff2a95016289e1af81e20ca08aef156e9ae5
-size 7286
+oid sha256:26515c23ad73a1752fe932ba4f87c522d3da91934872a085f975f84918e9197d
+size 36348
