4de0133d184f874471d8ab04c965dc3e9f0f1de8
tracer-ysyx <tracer@ysyx.org>
2025-08-07 15:21:19 +0800

    > sim RTL ysyx_25010003 彭柏淳 Linux mubai-Legion-R9000P-ARX8 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:19 up  6:20,  1 user,  load average: 0.99, 0.78, 0.74

--- npc/mynpc/csrc/memory/paddr.c
+++ npc/mynpc/csrc/memory/paddr.c
@@ -141,13 +141,14 @@ word_t paddr_read(paddr_t addr, int len) {
     }
   }
   // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  // out_of_bound(addr);
+  out_of_bound(addr);
   
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { 
+    printf("write " FMT_PADDR ", len: %d, data: " FMT_WORD "\n", addr, len, data);
     #ifdef CONFIG_MTRACE 
       write_log(addr, len, data);
     #endif
